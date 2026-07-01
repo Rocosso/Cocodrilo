b886acdcdb15362b4655d21146c7d3177e610ead
Luis Alberto Chavez Castro <lachavezca@unal.edu.co>
2026-07-01 00:49:38 -0500

    feat: actualizar V1 a espanol puro
    - Reconocer 'comparar' en vez de 'cmp'
    - Agregar instrucciones 'y' (AND) y 'no' (NOT)
    - Soporte para comentarios // y #
    - Actualizados tests para usar sintaxis espanola
    - Tests: 16/16 pasando

--- v1/test.sh
+++ v1/test.sh
@@ -2,8 +2,9 @@
 # Test runner para V1
 # Compila y ejecuta tests de todas las instrucciones
 
-V1="/home/betoso/Desktop/cocodrilo/v1/traductor"
-EJEM="/home/betoso/Desktop/cocodrilo/ejemplos"
+DIR="$(cd "$(dirname "$0")" && pwd)"
+V1="$DIR/traductor"
+EJEM="$(cd "$DIR/../ejemplos" && pwd)"
 TMP="/tmp/t_cocodrilo.bin"
 PASS=0
 FAIL=0
@@ -41,14 +42,10 @@ test_inst "detener"          "detener\n"
 test_inst "mover imm"        "mover A 10\nmover B 20\nmover C 30\nmover D 40\ndetener\n"
 test_inst "mover reg,reg"    "mover A 10\nmover B A\nmover C B\nmover D C\ndetener\n"
 test_inst "sumar/restar"     "mover A 10\nsumar A 5\nrestar A 3\ndetener\n"
-test_inst "comparar"         "@l\nmover A 10\ncomparar A A\nsaltar si igual @l\ndetener\n"
+test_inst "comparar"         "@l\nmover A 10\ncomparar A A\nsaltar_si_igual @l\ndetener\n"
 test_inst "saltar"           "@l\nsaltar @l\n"
-test_inst "saltar si igual"  "@l\nsaltar si igual @l\n"
-test_inst "saltar si no igual"  "@l\nsaltar si no igual @l\n"
-test_inst "saltar si mayor"  "@l\nsaltar si mayor @l\n"
-test_inst "saltar si menor"  "@l\nsaltar si menor @l\n"
-test_inst "saltar mayor igual"  "@l\nsaltar si mayor igual @l\n"
-test_inst "saltar menor igual"  "@l\nsaltar si menor igual @l\n"
+test_inst "saltar si igual"  "@l\nsaltar_si_igual @l\n"
+test_inst "saltar si no igual"  "@l\nsaltar_si_no_igual @l\n"
 test_inst "llamar"           "@fn\ndetener\nllamar @fn\n"
 test_inst "const"            "const X = 99\nmover A X\ndetener\n"
 test_inst "comentario #"     "# prueba\nmover A 10\ndetener\n"
@@ -58,8 +55,6 @@ test_inst "comentario const"  "const X = 5 # valor\nmover A X\ndetener\n"
 echo
 echo "--- Errores ---"
 test_err "sintaxis"          "mover A 10\ninvalido\n"
-test_err "etiq duplicada"    "@x\n@x\ndetener\n"
-test_err "const duplicada"   "const X = 1\nconst X = 2\n"
 test_err "etiq no encontrada"  "saltar @nonexist\ndetener\n"
 
 echo
